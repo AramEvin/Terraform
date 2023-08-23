590160376097b9755107bc8005170b5a0a4a6b79
AramEvin <a.evinyan@itsec.am>
2023-08-23 23:08:49 +0400

    Change configs and add key name for connection

--- instance.tf
+++ instance.tf
@@ -2,9 +2,14 @@ resource "aws_instance" "amazon_linux" {
   ami = "ami-04e35eeae7a7c5883"
   instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.aws_sg.id] 
-  user_data = file("aws_ssh.sh")
- 
+  key_name      = aws_key_pair.amazon_linux_ssh_key.key_name
+
   tags = {
     Name = "Terraform AWS"
   }
+}
+
+resource "aws_key_pair" "amazon_linux_ssh_key" {
+  key_name   = "ssh_key.pub"
+  public_key = file("~/.ssh/Terraform.pub")
 }
--- output.tf
+++ output.tf
@@ -1,3 +1,14 @@
+variable "username" {
+  type    = string
+  default = "ec2-user"
+}
+
+output "instance_username" {
+  description = "default username linux"
+  value       = var.username
+}
+
+
 output "instance_public_ip" {
   description = "Public IP address of the EC2 instance"
   value       = aws_instance.amazon_linux.public_ip
