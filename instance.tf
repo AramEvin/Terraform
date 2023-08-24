060b42f5bd9f9225dc87f5a85f015b83ef323b0a
AramEvin <a.evinyan@itsec.am>
2023-08-24 11:56:59 +0400

    Update instance file add User data

--- instance.tf
+++ instance.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "amazon_linux" {
   ami = "ami-04e35eeae7a7c5883"
   instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.aws_sg.id] 
+  user_data = file("bash.sh")
   key_name      = aws_key_pair.amazon_linux_ssh_key.key_name
 
   tags = {
@@ -11,5 +12,5 @@ resource "aws_instance" "amazon_linux" {
 
 resource "aws_key_pair" "amazon_linux_ssh_key" {
   key_name   = "ssh_key.pub"
-  public_key = file("~/.ssh/Terraform.pub")
+  public_key = file("~/.ssh/Terraform_aws.pub")
 }
