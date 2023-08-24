09d33aaa43e56aad9b7462a8f998a800556c27b1
AramEvin <a.evinyan@itsec.am>
2023-08-24 13:04:56 +0400

    Update and Change ingress path to dynamic ingress

--- security_group.tf
+++ security_group.tf
@@ -2,18 +2,15 @@ resource "aws_security_group" "aws_sg" {
   name        = "aws_sg"
   description = "Allow SSH Port for connection"
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  
-  ingress {
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+  dynamic "ingress" {
+    for_each = var.sg_ports
+    iterator = port
+    content {
+      from_port   = port.value
+      to_port     = port.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
   }
 
   egress {
