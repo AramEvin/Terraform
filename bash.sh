304634279f3dc2b9818018fb9908fad60df5ba32
AramEvin <a.evinyan@itsec.am>
2023-08-24 18:08:50 +0400

    Change Bash.sh for nginx config

--- bash.sh
+++ bash.sh
@@ -1,6 +1,31 @@
 #!/bin/bash
-sudo yum update -y &&
-sudo yum upgrade -y &&
-sudo yum install -y nginx
-echo "Hello World" > /var/www/html/index.html
+
+sudo yum update -y && sudo yum upgrade -y && sudo yum install -y nginx
+
+sudo echo "<!DOCTYPE html>
+<html>
+<head>
+<title>Welcome to nginx!</title>
+<style>
+html { color-scheme: light dark; }
+body { width: 35em; margin: 0 auto;
+font-family: Tahoma, Verdana, Arial, sans-serif; }
+</style>
+</head>
+<body>
+<h1>Welcome to nginx!</h1>
+<p>If you see this page, the nginx web server is successfully installed and
+working. Further configuration is required.</p>
+
+<p>For online documentation and support please refer to
+<a href="http://nginx.org/">nginx.org</a>.<br/>
+Commercial support is available at
+<a href="http://nginx.com/">nginx.com</a>.</p>
+
+<p><em>Thank you for using nginx.</em></p>
+</body>
+</html>" > /usr/share/nginx/html/index.html
+
+sudo systemctl start nginx
+ 
 sudo systemctl restart nginx
